ec398321dcbe42379051dda2e1f5979f6cef41bb
Maissa-benyaala <maissa.benyaala@gmail.com>
2022-03-25 18:33:19 +0100

    Done

--- src/data/data.js
+++ src/data/data.js
@@ -13,7 +13,7 @@ const data = [
         name: "Mixed",
         productUrl: "https://giftflowers.com.hk/media/catalog/product/cache/95bd47fa48d258c45b3c59bf581f9d30/m/y/mystery_love_1_1.jpeg",
         price: {
-            amount: 50,
+            amount: 80,
             currency: "TND"               
         } 
     },
@@ -22,7 +22,7 @@ const data = [
         name: "Green",
         productUrl: "https://giftflowers.com.hk/media/catalog/product/cache/54862d0ff29183830dfd9045b0db4c1a/l/i/little_flower_bouquet_1.jpg",
         price: {
-            amount: 55,
+            amount: 65,
             currency: "TND"               
         } 
     },
@@ -37,7 +37,7 @@ const data = [
     },
     { 
         id:5, 
-        name: "Bouquet1",
+        name: "Mixed",
         productUrl: "https://giftflowers.com.hk/media/catalog/product/cache/95bd47fa48d258c45b3c59bf581f9d30/m/y/mystery_love_1_1.jpeg",
         price: {
             amount: 50,
@@ -46,79 +46,79 @@ const data = [
     },
     { 
         id:6, 
-        name: "Bouquet1",
+        name: "Fancy Pink",
         productUrl: "https://cdn.shopify.com/s/files/1/2601/3352/products/0620-SweetGarden-Thumbnail-01_1200x.jpg?v=1626848221",
         price: {
-            amount: 50,
+            amount: 45,
             currency: "TND"               
         } 
     },
     { 
         id:7, 
-        name: "Bouquet1",
+        name: "Pink",
         productUrl: "https://i.pinimg.com/originals/d7/4e/27/d74e2746c6fa2f6d80baf0192c6c5195.jpg",
         price: {
-            amount: 50,
+            amount: 55,
             currency: "TND"               
         } 
     },
     { 
         id:8, 
-        name: "Bouquet1",
+        name: "Violet",
         productUrl: "https://www.fareastflora.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/p/e/pe48_revised.jpg",
         price: {
-            amount: 50,
+            amount: 75,
             currency: "TND"               
         } 
     },
     { 
         id:9, 
-        name: "Bouquet1",
-        productUrl: "https://cdn.shopify.com/s/files/1/2601/3352/products/PH-bouquet-rename-LR-04_1200x.jpg?v=1573805355",
+        name: "Pink",
+        productUrl: "https://i.pinimg.com/originals/53/90/93/539093667916645dd073af4500c8b454.jpg",
         price: {
-            amount: 50,
+            amount: 60,
             currency: "TND"               
         } 
     },
     { 
         id:10, 
-        name: "Bouquet1",
+        name: "Pink",
         productUrl: "https://www.warmestflowers.com/wp-content/uploads/2018/09/HB-303.jpg",
         price: {
-            amount: 50,
+            amount: 60,
             currency: "TND"               
         } 
     },
     { 
         id:11, 
-        name: "Bouquet1",
+        name: "Red",
         productUrl: "https://cdn11.bigcommerce.com/s-a4z7rskb8w/images/stencil/original/products/379/1727/romance-24-rose-bouquet-burnaby-bc-adeleraeflorist-ar2035__19562.1645038206.jpg?c=2",
         price: {
-            amount: 50,
+            amount: 55,
             currency: "TND"               
         } 
     },
     { 
         id:12, 
-        name: "Bouquet1",
+        name: "White",
         productUrl: "https://cdn.shopify.com/s/files/1/1264/6409/products/1-dozen-white-roses-davaoshop_1024x.jpg?v=1582188291",
         price: {
-            amount: 50,
+            amount: 70,
             currency: "TND"               
         } 
     },
     { 
         id:13, 
-        name: "Bouquet1",
-        productUrl: "https://giftflowers.com.hk/media/catalog/product/cache/54862d0ff29183830dfd9045b0db4c1a/l/i/little_flower_bouquet_1.jpg",
+        name: "Red",
+        productUrl: "https://cf.shopee.com.my/file/efd67488aa96871413c8da49b494fb99",
         price: {
-            amount: 50,
+            amount: 75,
             currency: "TND"               
         } 
     },
     { 
         id:14, 
-        name: "Bouquet1",
+        name: "White",
         productUrl: "https://www.juneflowers.ae/pub/media/catalog/product/cache/c0bb400db441ec67b37045c5a66e35a8/d/e/designer-bouquet-of-12-white-roses.jpg",
         price: {
             amount: 50,
@@ -127,7 +127,7 @@ const data = [
     },
     { 
         id:15, 
-        name: "Bouquet1",
+        name: "Pink",
         productUrl: "https://mflorist.hk/wp-content/uploads/Angelic-flower-bouquet-mood-shot-864x960.jpg",
         price: {
             amount: 50,
